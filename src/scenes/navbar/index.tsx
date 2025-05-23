245f888c3c30a7c8f7746fb46d126846422adedf
Mohammad <m.asgari91@gmail.com>
2025-05-23 19:50:05 +0430

    init: create fitness app with React and TypeScript and Tailwindcss

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,11 @@
+import { Bars3Icon,XMarkIcon } from "@heroicons/react/24/solid"
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>Navbar</div>
+    
+  )
+}
+
+export default Navbar
