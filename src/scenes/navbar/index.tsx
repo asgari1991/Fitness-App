023d8a091d16dc9472c9f8be985479a42937c768
Mohammad <m.asgari91@gmail.com>
2025-05-29 15:36:04 +0430

    feat:navbar menu

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,40 @@
-import { Bars3Icon,XMarkIcon } from "@heroicons/react/24/solid"
-type Props = {}
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import type { SelectedPage } from "@/shared/types";
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage,setSelectedPage}: Props) => {
+  const flexBetween = "flex items-center justify-between";
   return (
-    <div>Navbar</div>
-    
-  )
-}
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* Left Side*/}
+            <img src={Logo} alt="Logo" />
 
-export default Navbar
+            {/* Right Side*/}
+            <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                    <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                    <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                    <Link page="Our classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                    <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                    <p>Sign in</p>
+                    <button>Become a Member</button>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
