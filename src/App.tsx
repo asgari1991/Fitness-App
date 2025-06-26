90999e95d6aba50153a6bf7b7f3a1502a64320e8
Mohammad <m.asgari91@gmail.com>
2025-06-26 14:54:04 +0430

    feat:add responsive navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Navbar from "./scenes/navbar"
 import { SelectedPage } from "@/shared/types"
 
@@ -6,11 +6,26 @@ import { SelectedPage } from "@/shared/types"
 
 function App() {
 const [selectedPage,setSelectedPage]=useState<SelectedPage>(SelectedPage.Home)
+const [isTopOfPage,setIsTopOfPage]=useState<boolean>(true)
+useEffect(() => {
+  const handleScroll=()=>{
+    if (window.scrollY===0){
+      setIsTopOfPage(true)
+      setSelectedPage(SelectedPage.Home)
+    }else{
+      setIsTopOfPage(false)
+    }
+
+  }
+  window.addEventListener('scroll',handleScroll)
+return ()=>window.removeEventListener('scroll',handleScroll)
+
+}, [])
 
   return (
   
       <div className='app bg-gray-20'>
-        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+        <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
       </div>
       
    
